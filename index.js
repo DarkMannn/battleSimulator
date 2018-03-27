bf424a8d01cacf4f9810d68ebce330e7abebc9b1
DarkMannn <mdarko1993@gmail.com>
2018-03-28 00:10:37 +0200

    Fixed everything according to critical assessment

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const { Red, Blue, startSimulation } = require('./lib/simulator');
+const Army = require('./lib/formations/army');
+const startSimulation = require('./lib/simulator');
 
-const armyRed = Red.spawnArmy('strongest', { health: 4, experience: 3 });
-armyRed.spawnSquad(3, { type: 'soldier', recharge: 300, experience: 20 });
-Red.spawnArmy();
-Red.spawnArmy();
-Red.spawnArmy();
-
-const armyBlue = Blue.spawnArmy('weakest', { damage: 3 });
-armyBlue.spawnSquad(10, { type: 'vehicle', operators: { recharge: 300, experience: 20 } });
-Blue.spawnArmy();
-Blue.spawnArmy();
-Blue.spawnArmy();
-
-startSimulation();
